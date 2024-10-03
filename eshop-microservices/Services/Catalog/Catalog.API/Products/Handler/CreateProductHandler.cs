57bcc3a752dbba49a1e7960f4e2de539f9f62069
danielhagos11 <danielhh2008@gmail.com>
2024-10-03 22:39:10 +0100

    WIP

--- eshop-microservices/Services/Catalog/Catalog.API/Products/Handler/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/Handler/CreateProductHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Catalog.API.Data;
+using Catalog.API.Extensions;
 using Catalog.API.Products.Command;
+using OpenTelemetry.Trace;
 
 namespace Catalog.API.Products.Handler
 {
@@ -7,19 +9,23 @@ namespace Catalog.API.Products.Handler
     public class CreateProductHandler : IRequestHandler<CreateProductRequest, Product>
     {
         private readonly ProductCatalogDbContext _context;
-        public CreateProductHandler(ProductCatalogDbContext context)
+        private readonly ProductValidator _validator;
+        public CreateProductHandler(ProductCatalogDbContext context, ProductValidator validator)
         {
             _context = context;
+            _validator = validator;
         }
 
         public async Task<Product> Handle(CreateProductRequest request, CancellationToken cancellationToken)
         {
+          
             var product = request.ProductRequest;
-            if (_context.Products == null) 
+            var validateResult = _validator.Validate(product);
+            if (validateResult.IsValid)
             {
-                return null;
+                await _context.AddAsync(product);
             }
-            await _context.Products.AddAsync(product);
+           
             await _context.SaveChangesAsync();
             return product;
         }
--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,10 @@
+using BuildingBlocks.Behaviours;
 using Catalog.API.Data;
+using Catalog.API.Products;
+using Catalog.API.Products.Command;
 using Catalog.API.Services;
+using JasperFx.CodeGeneration.Frames;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,11 +17,15 @@ builder.Services.AddScoped<IProductCatalogRepository, ProductRepository>();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+//builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddDbContext<ProductCatalogDbContext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
 });
+builder.Services.AddScoped<IValidator<Product>, ProductValidator>();
 
 //builder.Services.AddMarten(opts =>
 //{
