2917db9de67f1e69f33cda01ffdf439d140f47a2
danielhagos11 <danielhh2008@gmail.com>
2024-10-04 10:54:46 +0100

    Added global exception handling into the program entry point

--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -4,7 +4,9 @@ using Catalog.API.Products;
 using Catalog.API.Products.Command;
 using Catalog.API.Services;
 using JasperFx.CodeGeneration.Frames;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -40,4 +42,30 @@ app.MapCarter();
 
 app.MapControllers();
 
+//app.UseExceptionHandler();
+
+//app.UseExceptionHandler(exceptionHandler =>
+//{
+//    exceptionHandler.Run(async context =>
+//    {
+//        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+//        if(exception == null)
+//        {
+//            return;
+//        }
+//        var problemDetails = new ProblemDetails
+//        {
+//            Title = exception.Message,
+//            Status = StatusCodes.Status500InternalServerError,
+//            Detail = exception.StackTrace
+//        };
+//        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+//        logger.LogError(exception, exception.Message);
+
+//        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+//        context.Response.ContentType = "application/problem+json";
+
+//        await context.Response.WriteAsJsonAsync(problemDetails);
+//    });
+//});
 app.Run();
